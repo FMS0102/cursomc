4708c2e8384e8ccba816b358005eb0c56a0e2191
FMS0102 <felsimoes@gmail.com>
2020-05-17 12:24:06 -0300

    Projeto Criado

---
+++ src/test/java/com/fms/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
